d77f797952a1c531776acf6cad54e0d3b71ae28b
matabear21 <59247918+matabear21@users.noreply.github.com>
2020-01-14 22:47:42 +0900

    last

--- app/src/main/java/com/example/hello/Tutorial1.java
+++ app/src/main/java/com/example/hello/Tutorial1.java
@@ -1,29 +1,77 @@
 package com.example.hello;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Tutorial1 extends AppCompatActivity {
-
     TextView textView;
+    TextView textView1;
+    Button button;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.tutorial1);
         textView = findViewById(R.id.tutorial1);
-        AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
-        AlphaAnimation fadeOut = new AlphaAnimation(1.0f, 0.0f);
-        textView.startAnimation(fadeIn);
-        textView.startAnimation(fadeOut);
-        fadeIn.setDuration(1200);
-        fadeIn.setFillAfter(true);
-        fadeOut.setDuration(1200);
-        fadeOut.setFillAfter(true);
-        fadeOut.setStartOffset(4200 + fadeIn.getStartOffset());
+        textView1 = findViewById(R.id.tutorial2);
+        button = findViewById(R.id.button);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("start",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        //editor.putInt("start",0);
+        //editor.apply();
+        //editor.commit();
+        Log.v("key","key"+sharedPreferences.getInt("start",0));
+
+        if(sharedPreferences.getInt("start",0)==0){
+            editor.putInt("start",1);
+            editor.apply();
+            Log.v("key","key"+sharedPreferences.getInt("start",0));
+
+            final AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+            final AlphaAnimation fadeIn1 = new AlphaAnimation(0.0f, 1.0f);
+            final AlphaAnimation fadeIn2 = new AlphaAnimation(0.0f, 1.0f);
+
+            fadeIn1.setDuration(1200);
+            fadeIn.setDuration(1200);
+            fadeIn2.setDuration(1200);
+
+
+
+            textView.startAnimation(fadeIn);
+            textView1.startAnimation(fadeIn1);
+            button.startAnimation(fadeIn2);
+            fadeIn1.setStartOffset(1200+fadeIn.getStartOffset());
+            fadeIn2.setStartOffset(1200+fadeIn1.getStartOffset());
+
+            editor.commit();
+        }
+        else {
+            Log.v("key","key"+sharedPreferences.getInt("start",0));
+            Intent intent = new Intent(this,Tutorial2.class);
+            startActivity(intent);
+            finish();
+        }
+
+        button.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Tutorial2.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+
     }
+
 }
--- app/src/main/java/com/example/hello/Tutorial2.java
+++ app/src/main/java/com/example/hello/Tutorial2.java
@@ -1,4 +1,76 @@
 package com.example.hello;
 
-public class Tutorial2 {
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Tutorial2 extends AppCompatActivity {
+    TextView textView;
+    TextView textView1;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.tutorial2);
+        textView = findViewById(R.id.tutorial3);
+        textView1 = findViewById(R.id.tutorial4);
+        button = findViewById(R.id.button2);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("start",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        //editor.putInt("start",0);
+        //editor.apply();
+        //editor.commit();
+
+
+        if(sharedPreferences.getInt("start2",0)==0){
+            editor.putInt("start2",1);
+            editor.apply();
+            Log.v("key","key"+sharedPreferences.getInt("start2",0));
+
+            final AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+            final AlphaAnimation fadeIn1 = new AlphaAnimation(0.0f, 1.0f);
+            final AlphaAnimation fadeIn2 = new AlphaAnimation(0.0f, 1.0f);
+
+            fadeIn1.setDuration(1200);
+            fadeIn.setDuration(1200);
+            fadeIn2.setDuration(1200);
+
+
+
+            textView.startAnimation(fadeIn);
+            textView1.startAnimation(fadeIn1);
+            button.startAnimation(fadeIn2);
+            fadeIn1.setStartOffset(1200+fadeIn.getStartOffset());
+            fadeIn2.setStartOffset(1200+fadeIn1.getStartOffset());
+
+            editor.commit();
+        }
+        else {
+            Log.v("key","key"+sharedPreferences.getInt("start2",0));
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        button.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+
+    }
 }
